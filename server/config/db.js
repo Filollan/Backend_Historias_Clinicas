4f061e7ed937b83bf70a3f34eb8accea534d235b
Filollan <mllanten@unimayor.edu.co>
2025-01-29 10:51:06 -0500

    cambios en la base de datos

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      `mongodb+srv://mllanten:q8VbJvuiz32KHGfO@cluster0.8ow5j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://mllanten:S5JlSh8U83A4CRuu@cluster0.8ow5j.mongodb.net/`
     )
 
     console.log(`MongoDB Conectado`)
